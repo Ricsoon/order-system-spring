8124e338edec6bb0d9d477e851133f3840990bbc
Ricardo Vinicius <painonex2@gmail.com>
2022-01-03 14:25:32 -0300

    Classe de Pagamentos

--- src/main/java/com/horizoncorp/ordersystem/config/TestConfig.java
+++ src/main/java/com/horizoncorp/ordersystem/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.horizoncorp.ordersystem.entities.Category;
 import com.horizoncorp.ordersystem.entities.Order;
 import com.horizoncorp.ordersystem.entities.OrderItem;
+import com.horizoncorp.ordersystem.entities.Payment;
 import com.horizoncorp.ordersystem.entities.Product;
 import com.horizoncorp.ordersystem.entities.User;
 import com.horizoncorp.ordersystem.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
