2a8b4fbc252ffa5b72d527890ecb02ba8ca35273
Ricardo Vinicius <painonex2@gmail.com>
2022-01-05 14:54:58 -0300

    Tratamendo de exceções - busca por ID

--- src/main/java/com/horizoncorp/ordersystem/services/UserService.java
+++ src/main/java/com/horizoncorp/ordersystem/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.horizoncorp.ordersystem.entities.User;
 import com.horizoncorp.ordersystem.repositories.UserRepository;
+import com.horizoncorp.ordersystem.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id){
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
